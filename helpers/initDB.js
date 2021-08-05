af0b06c3145ad7b4d871e57e404adea26622db8f
Tusharshrivastav <ts3657@gmail.com>
2021-08-05 13:09:41 +0530

    first commit

---
+++ helpers/initDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+function initDB(){
+    if(mongoose.connections[0].readyState){
+        console.log("alredy connected")
+        return
+    }
+    mongoose.connect(process.env.DB,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    mongoose.connection.on('connected',()=>{
+        console.log("connected to mongo")
+    })
+    mongoose.connection.on('error',(err)=>{
+        console.log("error connecting",err)
+    })
+}
+
+
+export default initDB
